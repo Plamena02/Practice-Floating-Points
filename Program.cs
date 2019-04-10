31267e8fe950e8e3f1f8442d67a8de70c5cdb6de
Plamena Nickolova <47273192+Plamena02@users.noreply.github.com>
2019-04-10 21:48:43 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Числа_с_плаваща_запетая
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = decimal.Parse(Console.ReadLine());
+            var b = double.Parse(Console.ReadLine());
+            var c = decimal.Parse(Console.ReadLine());
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
